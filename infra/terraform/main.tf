5776896e40767fe3f62eb0099df87f504b4e04c9
Peter Zhang <haoyang056@icloud.com>
2026-03-27 14:04:59 +0800

    infra: add prometheus and grafana monitoring stack

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -78,3 +78,25 @@ resource "helm_release" "kafka" {
 
   depends_on = [kubernetes_namespace.xianyuflow]
 }
+
+# Monitoring Namespace
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+# Prometheus Helm Release
+resource "helm_release" "prometheus" {
+  name       = "prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "55.0.0"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  values = [
+    file("${path.module}/../helm/xianyuflow-infra/values-monitoring.yaml")
+  ]
+
+  depends_on = [kubernetes_namespace.monitoring]
+}
